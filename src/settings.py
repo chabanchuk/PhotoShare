76f51ebb460df955589ae4ceee11d0302c07f655
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-05-16 10:51:15 +0300

    dev.env

---
+++ src/settings.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# Development settings
+dot_env_path = Path(__file__).parent / 'dev.env'
+# Production settings
+# dot_env_path = Path(__file__).parent / '.env'
+
+
+class EnvSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=dot_env_path,
+                                      env_file_encoding='utf-8',
+                                      extra='ignore')
+
+    sqlalchemy_url: str
+    secret_256: str
+    secret_512: str
+    access_algorithm: str
+    refresh_algorithm: str
+    mail_user: str
+    mail_pass: str
+    mail_server: str
+    mail_port: int
+    mail_from: str
+    cloudinary_url: str
+
+
+# production environment
+settings = EnvSettings()
+
