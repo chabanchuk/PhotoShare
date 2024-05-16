00fa54e26734a08e5aa163b4bfdbf218060e7945
Dmytro Tarasenko <ug.dotenko@gmail.com>
2024-05-16 20:49:41 +0300

    frontend_htmx_intro

--- src/frontend/routes.py
+++ src/frontend/routes.py
@@ -1,3 +1,22 @@
+from pathlib import Path
+
 from fastapi.routing import APIRouter
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+
+templates_path = Path(__file__).parent / 'templates'
+
+templates = Jinja2Templates(directory=str(templates_path))
+
+
+@router.get('/')
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@router.get('/htmx-test')
+def htmx_test(request: Request):
+    return HTMLResponse("<h1>Hello from htmx</h1>")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 import uvicorn
+from fastapi.staticfiles import StaticFiles
 
 from auth.routes import router as auth_router
 from comment.routes import router as comment_router
@@ -8,6 +11,9 @@ from user_profile.routes import router as user_router
 from frontend.routes import router as frontend_router
 
 app = FastAPI()
+
+static_path = Path(__file__).parent / 'frontend' / 'static'
+app.mount("/static", StaticFiles(directory=static_path), name='static')
 
 app.include_router(auth_router)
 app.include_router(comment_router)
