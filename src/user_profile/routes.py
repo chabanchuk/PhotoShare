525cb533a68047a2984cbb9fc7f1025c7bb94fd2
Dmytro Tarasenko <ug.dotenko@gmail.com>
2024-05-18 14:39:29 +0300

    alembic version added

--- src/user_profile/routes.py
+++ src/user_profile/routes.py
@@ -1,3 +1,29 @@
 from fastapi.routing import APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/user", tags=["user profile"])
+
+
+@router.get("/profile/{user_id}")
+async def get_user_profile(user_id: str):
+    return {"message": "User profile - public part"}
+
+
+@router.get("/profile/me")
+async def get_my_profile():
+    return {"message": "My profile"}
+
+
+@router.post("/profile/me")
+async def ceate_my_profile():
+    return {"message": "My profile created"}
+
+
+@router.put("/profile/me/{field}")
+async def update_my_profile(field: str,
+                            value: str):
+    return {"message": f"My profile field {field} updated"}
+
+@router.patch("/profile/me/{field}")
+async def patch_my_profile(field: str,
+                           value: str):
+    return {"message": f"My profile field {field} patched"}
