e55245a2402f2c1014c92db64ffc49d86030d78d
Dmytro Tarasenko <ug.dotenko@gmail.com>
2024-05-18 17:26:06 +0300

    user profile routes plus db with one user_profile

--- src/user_profile/routes.py
+++ src/user_profile/routes.py
@@ -1,6 +1,35 @@
+from typing import List, Annotated, Any
+
+from fastapi import Depends, status
 from fastapi.routing import APIRouter
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.responses import JSONResponse
+
+from database import get_db
+from auth.service import Authentication as auth_service
+from user_profile.model import UserPublicProfileModel
+from user_profile.orm import ProfileORM, UserORM
+
 
 router = APIRouter(prefix="/user", tags=["user profile"])
+
+
+@router.get("/profiles", response_model=List[UserPublicProfileModel])
+async def get_all_profiles(
+        db: Annotated[AsyncSession, Depends(get_db)],
+        offset: int = 0,
+        limit: int = 10
+) -> Any:
+    stmnt = select(ProfileORM).offset(offset).limit(limit)
+    res = await db.execute(stmnt)
+    res = res.scalars().all()
+    if len(res) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"detail": "No profiles found"}
+        )
+    return [UserPublicProfileModel.from_orm(profile) for profile in res]
 
 
 @router.get("/profile/{user_id}")
@@ -22,6 +51,7 @@ async def ceate_my_profile():
 async def update_my_profile(field: str,
                             value: str):
     return {"message": f"My profile field {field} updated"}
+
 
 @router.patch("/profile/me/{field}")
 async def patch_my_profile(field: str,
