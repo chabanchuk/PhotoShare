20ef957d04fd40c915ec9d4679e29d91c70a5b65
Dmytro-Tarasenko <147235829+Dmytro-Tarasenko@users.noreply.github.com>
2024-05-22 19:05:39 +0300

    Update require_role.py

--- src/auth/require_role.py
+++ src/auth/require_role.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException, status
 from userprofile.orm import UserORM
 from auth.service import Authentication
 
-def require_role(allowed_roles: list[str]):
+def require_role(allowed_roles: list[str] = ["user"]):
     def role_dependency(current_user: UserORM = Depends(Authentication().get_access_user)):
         if current_user.role not in allowed_roles:
             raise HTTPException(
