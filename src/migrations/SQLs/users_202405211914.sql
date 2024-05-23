dd6e083f684313edf33cb5710b5e08fa695442fb
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-05-23 08:03:14 +0300

    UserProfile working

--- src/migrations/SQLs/users_202405211914.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (id,email,username,password,loggedin,registered_at,"role") VALUES
-	 (1,'user@example.com','petryk_pyatochkin','$2b$12$cqYaDnrxQ1BacamD3JYJKuLJRzOUzazkeJChWrJdhh1yFpvbXow46',0,'2024-05-20 07:35:04.977368','admin'),
-	 (2,'vasyl@example.com','vasyl_pupkin','$2b$12$IgFpOjgsjlmaHrEr.aKd.OMjVXfyZNi2B/xqmAg6fGExQjmewAtPW',0,'2024-05-20 07:37:46.199074','user'),
-	 (3,'viktor@example.com','big_voktor','$2b$12$VXlgPut0mtqc15BoUgrA0exkhuINVyu3euvxLrk.S941abmwuh4tC',1,'2024-05-20 08:19:15.590028','user');
