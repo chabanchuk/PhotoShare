54d281449a9433c1bcfbe07eb2aa868c388ce2fd
AnzhelikaKodlubovska <angelikagymnasticschannel@gmail.com>
2024-05-24 20:28:42 +0300

    create_faker_the_latest_option

--- src/create_fake_users.py
+++ src/create_fake_users.py
@@ -1,40 +1,33 @@
 import asyncio
-from datetime import datetime, timedelta
+from datetime import datetime
 from faker import Faker
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
 
 from userprofile.orm import UserORM, ProfileORM
-from database import get_db, Base  
+from database import get_db
 
 fake = Faker()
 
-DATABASE_URL = ""  
-
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
-
-async def create_fake_user_and_profile(session):
-    async with session() as db:
-        async with db.begin():
-            for _ in range(5):
+async def create_fake_user_and_profile():
+    async with get_db() as session:
+        async with session.begin():
+            for index in range(5):
                 email = fake.email()
                 username = fake.user_name()
-                password = fake.password()
-                registered_at = datetime.now(fake.timezone())
+                password = "password"
+                registered_at = datetime.now()
+
+                role = "admin" if index == 0 else "user"
 
                 user = UserORM(
                     email=email,
                     username=username,
                     password=password,
                     registered_at=registered_at,
-                    role="user"
+                    role=role
                 )
 
-                db.add(user)
-                await db.flush()  
+                session.add(user)
+                await session.flush()
                 first_name = fake.first_name()
                 last_name = fake.last_name()
                 birthday = fake.date_of_birth(minimum_age=18, maximum_age=90)
@@ -47,15 +40,10 @@ async def create_fake_user_and_profile(session):
                     user_id=user.id
                 )
 
-                db.add(profile)
-
-        await db.commit()
+                session.add(profile)
 
 async def main():
-    async with async_session() as session:
-        await create_fake_user_and_profile(session)
+    await create_fake_user_and_profile()
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
