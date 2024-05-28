abf61337110565c947a1a001afc2ef86570f4fa8
chabanchuk <145770156+chabanchuk@users.noreply.github.com>
2024-05-28 11:06:07 +0300

    change up

--- src/main.py
+++ src/main.py
@@ -41,4 +41,4 @@ async def call_response_modificator(request, call_next):
 if __name__ == "__main__":
     uvicorn.run(app=app,
                 host="0.0.0.0",
-                port=8000)
+                port=8080)
