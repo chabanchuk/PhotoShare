5f644d0880e95f5f43c4c299b6da86389d0e3812
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-05-29 12:16:31 +0300

    Frontend expansion

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 from fastapi import FastAPI
 import uvicorn
+import gunicorn
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth.routes import router as auth_router
 from email_service.routes import router as email_router
@@ -28,6 +30,19 @@ app.include_router(frontend_router)
 app.include_router(tags_router)
 
 
+origins = [
+    "https://goit-pg5-goit-pg5-3039617b.koyeb.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.middleware('http')
 async def call_header_cookie_crutch(request, call_next):
     return await crutches.cookie_to_header_jwt(request, call_next)
@@ -40,5 +55,5 @@ async def call_response_modificator(request, call_next):
 
 if __name__ == "__main__":
     uvicorn.run(app=app,
-                host="0.0.0.0",
+                host="localhost",
                 port=8000)
